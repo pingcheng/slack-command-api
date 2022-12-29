478b18c3cfe6ee342ea3885e8609298ef05b3fcf
Ping Cheng <neroc7@gmail.com>
2022-12-29 22:53:01 +1100

    add local deploy

--- serverless.ts
+++ serverless.ts
@@ -5,7 +5,11 @@ import app from '@functions/app';
 const serverlessConfiguration: AWS = {
   service: 'slack-command-api',
   frameworkVersion: '3',
-  plugins: ['serverless-esbuild', 'serverless-offline'],
+  plugins: [
+    'serverless-esbuild',
+    'serverless-offline',
+    'serverless-localstack',
+  ],
   provider: {
     name: 'aws',
     runtime: 'nodejs16.x',
@@ -17,6 +21,8 @@ const serverlessConfiguration: AWS = {
       AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
       NODE_OPTIONS: '--enable-source-maps --stack-trace-limit=1000',
     },
+    stage: 'local',
+    region: 'ap-southeast-2'
   },
   // import the function via paths
   functions: { app },
@@ -34,6 +40,9 @@ const serverlessConfiguration: AWS = {
     },
     'serverless-offline': {
       noPrependStageInUrl: true
+    },
+    localstack: {
+      stages: ['local']
     }
   },
 };
