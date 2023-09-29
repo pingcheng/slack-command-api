d5ff6caca477b291b9b79f81133015298df7a908
Ping Cheng <neroc7@gmail.com>
2023-09-30 01:11:41 +1000

    update lambda size

--- serverless.ts
+++ serverless.ts
@@ -1,40 +1,40 @@
-import type { AWS } from '@serverless/typescript';
+import type { AWS } from "@serverless/typescript";
 
-import app from '@functions/app';
+import app from "@functions/app";
 
 const serverlessConfiguration: AWS = {
-  service: 'slack-command-api',
-  frameworkVersion: '3',
+  service: "slack-command-api",
+  frameworkVersion: "3",
   useDotenv: true,
   plugins: [
-    'serverless-esbuild',
-    'serverless-offline',
-    'serverless-localstack',
+    "serverless-esbuild",
+    "serverless-offline",
+    "serverless-localstack",
   ],
   provider: {
-    name: 'aws',
-    runtime: 'nodejs16.x',
+    name: "aws",
+    runtime: "nodejs18.x",
     apiGateway: {
       minimumCompressionSize: 1024,
       shouldStartNameWithService: true,
     },
     environment: {
-      AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
-      NODE_OPTIONS: '--enable-source-maps --stack-trace-limit=1000',
+      AWS_NODEJS_CONNECTION_REUSE_ENABLED: "1",
+      NODE_OPTIONS: "--enable-source-maps --stack-trace-limit=1000",
     },
-    stage: 'local',
-    region: 'ap-southeast-2',
+    stage: "local",
+    region: "ap-southeast-2",
     iam: {
       role: {
         statements: [
           {
-            "Effect": "Allow",
-            "Action": "sqs:SendMessage",
-            "Resource": "${env:FUEL_PRICE_QUEUE_ARN}"
-          }
-        ]
-      }
-    }
+            Effect: "Allow",
+            Action: "sqs:SendMessage",
+            Resource: "${env:FUEL_PRICE_QUEUE_ARN}",
+          },
+        ],
+      },
+    },
   },
   // import the function via paths
   functions: { app },
@@ -45,17 +45,17 @@ const serverlessConfiguration: AWS = {
       minify: false,
       sourcemap: true,
       exclude: [],
-      target: 'node16',
-      define: { 'require.resolve': undefined },
-      platform: 'node',
+      target: "node16",
+      define: { "require.resolve": undefined },
+      platform: "node",
       concurrency: 10,
     },
-    'serverless-offline': {
-      noPrependStageInUrl: true
+    "serverless-offline": {
+      noPrependStageInUrl: true,
     },
     localstack: {
-      stages: ['local']
-    }
+      stages: ["local"],
+    },
   },
 };
 
--- src/functions/app/index.ts
+++ src/functions/app/index.ts
@@ -2,7 +2,7 @@ import { handlerPath } from "@libs/handler-resolver";
 
 export default {
   handler: `${handlerPath(__dirname)}/handler.server`,
-  memorySize: 256,
+  memorySize: 512,
   environment: {
     FUEL_PRICE_QUEUE_URL: "${env:FUEL_PRICE_QUEUE_URL}",
     SLACK_ACCESS_TOKEN: "${env:SLACK_ACCESS_TOKEN}",
